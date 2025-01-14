057ca85cdca4b0930ded1b2b3a50e3fe93095d76
Fabrici0 <94077616+Fabrici01@users.noreply.github.com>
2025-01-14 11:40:38 -0300

    Update algoritmos.h
    ;

--- algoritmos.h
+++ algoritmos.h
@@ -2,4 +2,4 @@ void bolha(int *vetor, int n);
 void bolhaComParada(int *vetor, int n);
 void insercaoDireta(int *vetor, int n);
 void insercaoBinaria(int *vetor, int n);
-void shellsort(int *vetor, int n)
+void shellsort(int *vetor, int n);
