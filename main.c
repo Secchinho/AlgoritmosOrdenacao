b7e8ff75dbf272869170c2c2d4cd7ea91b90698e
Fabrici0 <94077616+Fabrici01@users.noreply.github.com>
2025-01-22 17:17:38 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,15 +5,26 @@
 int main()
 {
     int op = 0;
-    printf("----------MENU----------\n");
-    printf("1 - Gerar números\n");
-    printf("2 - Ordenar números\n");
-    printf("------------------------\n");
-    printf("Opção: ");
-    scanf(" %d", &op);
+    do
+    {
+        printf("----------MENU----------\n");
+        printf("1 - Gerar numeros\n");
+        printf("2 - Ordenar numeros\n");
+        printf("3 - Sair\n");
+        printf("------------------------\n");
+        printf("Opcao: ");
+        scanf(" %d", &op);
 
-    system("clear");
-
-    menuTamEntrada(op);
+        system("cls");
+        if (op != 3)
+        {
+            menuTamEntrada(op);
+        }
+        else
+        {
+            printf("Saindo.....\n");
+        } 
+    } while (op != 3);
+    
     return 0;
 }
