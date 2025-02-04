5fce509b8d1e8cc68b841d8cb9688b78cb14d28a
Raphael Secchin <158609068+Secchinho@users.noreply.github.com>
2025-02-04 15:44:44 -0300

    Update radixSort.h

--- radixSort.h
+++ radixSort.h
@@ -1,3 +1,3 @@
-int maiorNumero(int *vetor, int n, long long int *comp);
-void contagem(int *vetor, int n, int exp, long long int *comp, long long int *troca);
+int maiorNumero(int *vetor, int n);
+void contagem(int *vetor, int n, int exp, long long int *troca);
 void radixsort(int *vetor, int n);
