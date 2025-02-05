d5cf0bef236263ceded6dd8aefd4b71ed74eb425
Raphael Secchin <158609068+Secchinho@users.noreply.github.com>
2025-02-05 17:11:47 -0300

    Update quickSortCentro.h

--- quickSortCentro.h
+++ quickSortCentro.h
@@ -1,3 +1,3 @@
 void particao(int *vetor, int esquerda, int direita, int *i, int *j, long long int *comp, long long int *troca);
-void ordena(int *vetor, int esquerda, int direita, long long int *comp, long long int *troca);
-void quicksortCentro(int *vetor, int esquerda, int direita);
+void quickSortCentro(int *vetor, int esquerda, int direita, long long int *comp, long long int *troca);
+void contadorQuickSortCentro(int *vetor, int esquerda, int direita);
