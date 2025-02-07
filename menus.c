a12a609139973e61ac55183c667dbe0b7238b656
Raphael Secchin <158609068+Secchinho@users.noreply.github.com>
2025-02-07 11:05:40 -0300

    Update menus.c

--- menus.c
+++ menus.c
@@ -116,7 +116,7 @@ void menuOrdenacao(int tamanho)
         break;
     case 9:
         // QuickSort Centro
-        contadorQuicksortCentro(vetor, 0, tamanho);
+        contadorQuickSortCentro(vetor, 0, tamanho);
         saida(vetor, tamanho, "QuicksortCentro");
         break;
     case 10:
